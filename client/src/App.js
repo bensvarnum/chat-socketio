7b2b2f3d27e685e18ad334c615030ede16e2f7d8
bensvarnum <bens.varnum@gmail.com>
2021-03-14 19:33:14 -0400

    setup join and chat componenet

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Join from './component/Join';
+import Chat from './component/Chat';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+<BrowserRouter>
+        <Route path='/' exact component={Join} />
+        <Route path='/chat' exact component={Chat} />
+    </BrowserRouter>
+    )
+    
 }
 
 export default App;
