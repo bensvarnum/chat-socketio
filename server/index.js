dd6567db878bca51c2e67548a187b3d8ddca34e0
bensvarnum <bens.varnum@gmail.com>
2021-03-14 19:57:27 -0400

    basic socket connection

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./routes/router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+    console.log('we are connected');
+
+    socket.on('disconnect', () => {
+        console.log('we out');
+    })
+})
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
