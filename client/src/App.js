fd1bcb7d9f4c93bbd23cd1abdea231f786d412cb
bensvarnum <bens.varnum@gmail.com>
2021-03-14 20:50:49 -0400

    login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Join from './component/Join';
-import Chat from './component/Chat';
+import Join from './component/Join/Join';
+import Chat from './component/Chat/Chat';
 
 const App = () => {
     return (
-<BrowserRouter>
+    <BrowserRouter>
         <Route path='/' exact component={Join} />
         <Route path='/chat' exact component={Chat} />
     </BrowserRouter>
